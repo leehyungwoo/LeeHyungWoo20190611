27be0e9d37a298ec81da54252dc89366a434149d
dlguddn3214 <dlguddn3214@gmail.com>
2019-06-12 14:27:48 +0900

    14:27

--- resources/static/resources/js/index.js
+++ resources/static/resources/js/index.js
@@ -3,8 +3,8 @@ var app = (function(){
         login_form()
       }
       let login_form =function(){
-            var wrapper = document.querySelector("#wrapper");
-            var loginFormTxt = '<form>' 
+            let wrapper = document.querySelector("#wrapper");
+            wrapper.innerHTML='<form>'
             +'  First name:<br>'
             +'  <input type="text" name="firstname" >'
             +'  <br>'
@@ -12,52 +12,41 @@ var app = (function(){
             +'  <input type="text" name="lastname" >'
             +'  <br><br>'
             +'  <button id="login" value="로그인">로그인</button>'
-            +'  <button id="signUp" value="회원가입">회원가입</button>'
+            +'  <button id="join" value="회원가입">회원가입</button>'
             +'</form> '
             ;
-            
-            wrapper.innerHTML=loginFormTxt;
 
-            var loginBtn = document.querySelector("#login");
-            var signUp = document.querySelector("#signUp");
-            loginBtn.addEventListener("click",function(){
-                alert("로그인 Y/N");
-            })
-    
-            signUp.addEventListener("click",function(){
+            let join_btn = document.getElementById('join')
+            join_btn.addEventListener('click',function(){
+                 join_form();
+             })
+        }
 
-                wrapper.innerHTML='<form>'
-                +'  id:<br>'
-                +'  <input type="text" name="u_id" >'
-                +'  <br>'
-                +'  password:<br>'
-                +'  <input type="password" name="password" >'
-                +'  <br>'
-                +'  name:<br>'
-                +'  <input type="text" name="name" >'
-                +'  <br>'
-                +'  email:<br>'
-                +'  <input type="text" name="email" >'
-                +'  <br>'
-                +'  phone:<br>'
-                +'  <input type="text" name="phone" >'
-                +'  <br>'
-                +'  <br><br>'
-                +'  <button id="confirm" value="회원가입">확인</button>'
-                +'  <button id="cancel" value="취소">취소</button>'
-                +'</form> '
-                ;
-                var confirm = document.querySelector('#confirm');
-                var cancel =document.querySelector('#cancel');
+    let join_form = function(){
+        let wrapper = document.querySelector("#wrapper");
+            wrapper.innerHTML='<form>'
+            +'  id:<br>'
+            +'  <input type="text" name="u_id" >'
+            +'  <br>'
+            +'  password:<br>'
+            +'  <input type="password" name="password" >'
+            +'  <br>'
+            +'  name:<br>'
+            +'  <input type="text" name="name" >'
+            +'  <br>'
+            +'  email:<br>'
+            +'  <input type="text" name="email" >'
+            +'  <br>'
+            +'  phone:<br>'
+            +'  <input type="text" name="phone" >'
+            +'  <br>'
+            +'  <br><br>'
+            +'  <button id="confirm" value="회원가입">확인</button>'
+            +'  <button id="cancel" value="취소">취소</button>'
+            +'</form> '
 
-                confirm.addEventListener('click',function(){
-                    wrapper.innerHTML=loginFormTxt;
-                })
-                cancel.addEventListener('click',function(){
-                    wrapper.innerHTML=loginFormTxt;
-                })
-            })
     }
+
     return {init:init}; 
 })();
 
